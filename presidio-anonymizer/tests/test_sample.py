da65d5e3b3cea5e728948a8475f7def656edbee4
Steven Loy <231585531+sloy1cosc381@users.noreply.github.com>
2025-09-30 03:16:59 +0000

    made test for test_sample

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,13 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
+from presidio_anonymizer import AnonymizerEngine
+from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
+
 def test_sample_run_anonymizer():
-    # replace the following line with your test
-    pass
+    result = AnonymizerEngine().anonymize(
+        text="My name is Bond.",
+        analyzer_results=[RecognizerResult(entity_type="PERSON", start=11, end=15, score=0.8)],
+        operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
+    )
+    assert sample_run_anonymizer("My name is Bond.", 11, 15)==result
